e4d235384443fd90f91945c74d6b4947f6a8e427
rbblazquez <rubenbarroso16@gmail.com>
2022-12-28 20:33:09 +0100

    new RandomForestClassifier added

--- HandsOnMachineLearningProjects/A3_Classification/BinaryClassification.py
+++ HandsOnMachineLearningProjects/A3_Classification/BinaryClassification.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import patches
 from sklearn.datasets import fetch_openml
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, precision_recall_curve
@@ -20,10 +21,13 @@ if __name__ == '__main__':
     print(x.shape, y.shape)
 
     some_digit = x[0]
+    some_false_digit = x[5]
     plot_digit(some_digit)
-    # plt.show()
+    plot_digit(some_false_digit)
+    plt.show()
 
     print(y[0])
+    print(y[5])
 
     x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
 
@@ -59,15 +63,19 @@ if __name__ == '__main__':
     # when we have more threshold, recall decreases and precision increase
 
     # we can use decision_function to compare with a single threshold
-    y_scores = sdg_clf.decision_function([some_digit])
+    y_score_some_digit = sdg_clf.decision_function([some_digit])
+    y_score_some_false_digit = sdg_clf.decision_function([some_false_digit])
+    print('y_score__some_digit', y_score_some_digit)
+    print('y_score__some_false_digit', y_score_some_false_digit)
     threshold = 0
-    y_some_digit_pred = (y_scores > threshold)  # we get the same result than the predict
+    y_some_digit_pred = (y_score_some_digit > threshold)  # we get the same result than the predict
+    y_some_digit_false_pred = (y_score_some_false_digit > threshold)  # we get the same result than the predict
 
     # now if we have to check multiple thresholds we can use this methods
     y_scores = cross_val_predict(sdg_clf, x_train, y_train_5, cv=3,
                                  method='decision_function',
                                  n_jobs=5)  # first we get all scores from all instances os the training set
-    print('y_scores', y_scores)
+
     precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 
     plt.figure(figsize=(8, 4))  # extra code – it's not needed, just formatting
@@ -86,11 +94,25 @@ if __name__ == '__main__':
 
     plt.show()
 
-    plt.figure(figsize=(8, 4))
-    plt.plot(recalls, precisions, linewidth=2, label='Precision/Recall Curve')
-    plt.plot([recalls[idx], recalls[idx]], [0, precisions[idx]], linewidth=2)
-    plt.plot([0, recalls[idx]], [precisions[idx], precisions[idx]], linewidth=2)
-    plt.plot()
+    plt.figure(figsize=(6, 5))  # extra code – not needed, just formatting
+
+    plt.plot(recalls, precisions, linewidth=2, label="Precision/Recall curve")
+    # extra code – just beautifies and saves Figure 3–6
+    plt.plot([recalls[idx], recalls[idx]], [0., precisions[idx]], "k:")
+    plt.plot([0.0, recalls[idx]], [precisions[idx], precisions[idx]], "k:")
+    plt.plot([recalls[idx]], [precisions[idx]], "ko",
+             label="Point at threshold 3,000")
+    plt.gca().add_patch(patches.FancyArrowPatch(
+        (0.79, 0.60), (0.61, 0.78),
+        connectionstyle="arc3,rad=.2",
+        arrowstyle="Simple, tail_width=1.5, head_width=8, head_length=10",
+        color="#444444"))
+    plt.text(0.56, 0.62, "Higher\nthreshold", color="#333333")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.axis([0, 1, 0, 1])
+    plt.grid()
+    plt.legend(loc="lower left")
     plt.show()
 
     # get threshold for a 90% precision
@@ -108,3 +130,8 @@ if __name__ == '__main__':
 
     print('precision 2', precision_score(y_train_5, y_train_recall_95))
     print('recall 2', recall_score(y_train_5, y_train_recall_95))
+
+    print('prediction with treshold with positive number', (y_score_some_digit > thresholds[idx_for_95_recall]),
+          sdg_clf.predict([some_digit]))
+    print('prediction with treshold with negative number ', (y_score_some_false_digit > thresholds[idx_for_95_recall]),
+          sdg_clf.predict([some_false_digit]))
--- HandsOnMachineLearningProjects/A3_Classification/RocCurve.py
+++ HandsOnMachineLearningProjects/A3_Classification/RocCurve.py
@@ -1,6 +1,9 @@
+import matplotlib.pyplot as plt
+from matplotlib import patches
 from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, precision_recall_curve, roc_auc_score
 from sklearn.model_selection import cross_val_predict
 
 if __name__ == '__main__':
@@ -23,4 +26,46 @@ if __name__ == '__main__':
                                  method='decision_function',
                                  n_jobs=5)
 
-    false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train_5, y)
+    precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+    false_positive_rate, true_positive_rate, thresholds_roc_curve = roc_curve(y_train_5, y_scores)
+    idx_threshold_precision_90 = (precisions > 0.90).argmax()
+
+    # we do less equal instead of greater equal because roc_curve returns thresholds
+    # from greater to lower
+    idx_for_threshold_at_90 = (thresholds <= thresholds[idx_threshold_precision_90]).argmax()
+    tpr_90, fpr_90 = false_positive_rate[idx_for_threshold_at_90], true_positive_rate[idx_for_threshold_at_90]
+
+    plt.plot(false_positive_rate, true_positive_rate, linewidth=2, label='ROC Curve')
+    plt.plot([0, 1], [0, 1], 'k:', label='Random Classifiers ROC Curve')
+    plt.plot(fpr_90, tpr_90, 'k:', label='Threshold for 90% precision')
+    plt.gca().add_patch(patches.FancyArrowPatch(
+        (0.2, 0.88), (0.07, 0.7),
+        connectionstyle="arc3,rad=.4",
+        arrowstyle="Simple, tail_width=1.5, head_width=8, head_length=10",
+        color="#444444"))
+    plt.grid()
+    plt.text(0.12, 0.71, "Higher\nthreshold", color="#333333")
+    plt.legend()
+    plt.axis([0, 1, 0, 1])
+    plt.show()
+
+    # to know if use roc_aux_score or precision_recall_curve, we have to know the number of positives is rare
+    # or you care about false_positives , you have to use pr_curve, if not, use ROC_curve
+    print(roc_auc_score(y_train_5, y_scores))
+
+    forest_clf = RandomForestClassifier(random_state=42)
+    y_proba_forest = cross_val_predict(forest_clf, x_train, y_train_5, cv=3, method='predict_proba', n_jobs=-1)
+    print(y_proba_forest[: 2])
+
+    # now we plot the last pr_curve and this forest pr_curve
+    # since the seconds column is the positive probabilities, que take that
+    y_scores_forest = y_proba_forest.iloc[:, 1]
+
+    precision_forest, recall_forests, thresholds_forest = precision_recall_curve(y_train_5, y_scores_forest)
+    plt.plot(recall_forests, precision_forest, 'b--', linewidth=2, label="Random Forests")
+    plt.plot(recalls, precisions, 'g--', linewidth=2, label="Random Forests")
+    plt.grid()
+    plt.xlabel('Recalls')
+    plt.ylabel('Precisions')
+    plt.legend()
+    plt.show()
